b621003ca672a56e713be0bcb0b614e83fc6db19
Abhay Kumar <abhayait@gmail.com>
2014-09-08 23:39:26 +0530

    commit fixes

--- db/migrate/20140817063131_create_user_event_infos.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserEventInfos < ActiveRecord::Migration
-  def change
-    create_table :user_event_infos do |t|
-      	
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140818115118) do
     t.string   "mobile_number", limit: 11
     t.string   "firstname",     limit: 20
     t.string   "lastname",      limit: 20
+    t.string   "username",      limit: 20
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password",      limit: 20
@@ -49,5 +50,6 @@ ActiveRecord::Schema.define(version: 20140818115118) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
