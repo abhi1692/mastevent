907ec8d6cbb9d7fc524621162a6286d2638470c3
Abhay Kumar <abhayait@gmail.com>
2014-09-09 00:56:36 +0530

    commit fixes

--- db/migrate/20140804194811_create_events.rb
+++ db/migrate/20140804194811_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration
     create_table :events do |t|
       t.string :event_name
       t.string :date_of_event
-      t.string :coordinator_required
+      t.integer :coordinator_required
       t.string :pay_per_day
 
       t.timestamps
--- db/migrate/20140804195514_update_events.rb
+++ db/migrate/20140804195514_update_events.rb
@@ -2,7 +2,6 @@ class UpdateEvents < ActiveRecord::Migration
   def change
   	change_column :events, :event_name, :string, :limit =>100
   	change_column :events, :date_of_event,:string, :limit =>15
-  	change_column :events, :coordinator_required,:integer, :limit=>4
   	change_column :events, :pay_per_day, :string, :limit =>4
 
   end 	
