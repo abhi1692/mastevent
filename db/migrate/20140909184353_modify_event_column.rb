0a62951fc897e3440f6df235623e736c4ab92512
abhi1692 <kumarabhinav.1692@gmail.com>
2014-09-10 11:05:15 +0530

    migration fix

--- db/migrate/20140909184353_modify_event_column.rb
+++ db/migrate/20140909184353_modify_event_column.rb
@@ -1,6 +1,7 @@
 class ModifyEventColumn < ActiveRecord::Migration
   def change
-    change_column :events, :date_of_event, :date
+    #change_column :events, :date_of_event, :date
+    execute "ALTER TABLE events ALTER COLUMN date_of_event TYPE date"
     rename_column :events, :date_of_event, :event_start_date
     change_column :events, :pay_per_day, :decimal, :precision => 15, :scale => 2
   end
