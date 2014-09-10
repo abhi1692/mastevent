067338310db36f0b8ef46b3ff871858cd0d9972e
Abhay Kumar <abhayait@gmail.com>
2014-09-10 23:19:14 +0530

    commit fixes

--- app/models/user_event_info.rb
+++ app/models/user_event_info.rb
@@ -21,11 +21,11 @@ class UserEventInfo < ActiveRecord::Base
     user_event_info_obj.address = params[:address]
     user_event_info_obj.event_exprience=params[:event_exp]
     user_event_info_obj.preferred_location =params[:preferred_location]
-    user_event_info_obj.suitable_day= params[:suitable_day].join(', ').to_s
+    user_event_info_obj.suitable_day= (params[:suitable_day] || []).join(', ').to_s
     user_event_info_obj.suitable_time_from = params[:suitable_time_from]
     user_event_info_obj.suitable_time_to = params[:suitable_time_to]
     user_event_info_obj.bike = params[:vehicle]
-    user_event_info_obj.qualification= params[:qualification].join(', ').to_s
+    user_event_info_obj.qualification= (params[:qualification] || []).join(', ').to_s
     user_event_info_obj.more_details= params[:more_details]
     user_event_info_obj.save!
     user_event_info_obj
