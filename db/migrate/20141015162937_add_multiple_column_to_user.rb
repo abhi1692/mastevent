f3ce761325e7e3e9c6a86b52b9210bc5a218208e
abhi1692 <kumarabhinav.1692@gmail.com>
2014-10-23 00:28:32 +0530

    migration fix

--- db/migrate/20141015162937_add_multiple_column_to_user.rb
+++ db/migrate/20141015162937_add_multiple_column_to_user.rb
@@ -1,6 +1,6 @@
 class AddMultipleColumnToUser < ActiveRecord::Migration
   def change
-  	add_column :users, :alternate_mobile_number, :integer, :limit => 11
+  	add_column :users, :alternate_mobile_number, :integer, :limit => 6
     add_column :users, :city, :string
     add_column :users, :nationality, :string
     add_column :users, :language, :string
