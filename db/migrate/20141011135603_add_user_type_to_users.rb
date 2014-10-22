32e4c73a1d9a9902b47dbacb6cd103d6c7ae443f
abhi1692 <kumarabhinav.1692@gmail.com>
2014-10-23 00:20:54 +0530

    migration fix

--- db/migrate/20141011135603_add_user_type_to_users.rb
+++ db/migrate/20141011135603_add_user_type_to_users.rb
@@ -1,6 +1,6 @@
 class AddUserTypeToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :user_type, "tinyint(3)", :default => 0, :null => false
+    add_column :users, :user_type, :integer, :limit => 4,:default => 0, :null => false
     remove_column :users, :username, :string
     add_column :users, :username, :string, :limit => 255, :null => false, :after => :lastname
   end
